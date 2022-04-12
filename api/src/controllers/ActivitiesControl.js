6021ad8533dfb44767890f14ebb2a6128a2f35f3
Your Name <you@example.com>
2022-04-12 10:18:41 -0300

    OK backend and database

--- api/src/controllers/ActivitiesControl.js
+++ api/src/controllers/ActivitiesControl.js
@@ -1,7 +1,28 @@
 const axios = require ('axios');
+const {Activity, Country} = require ('../db')
 
-const createActivity = () => {
 
+//ANDA! HAY QUE REVISAR TEORÍA PARA ESTÁR SEGURA EN ESTOS TEMAS
+const createActivity = async( req, res) => {
+ try{
+    const {name, difficulty, duration, season, countries} = req.body 
+    const newActivity = await Activity.create ({
+        name,
+        difficulty, 
+        duration, 
+        season
+    })
+    let countriesDb = await Country.findAll({
+        where: { name: countries },
+      })
+
+    await newActivity.addCountries(countriesDb);
+    // console.log (newActivity)
+    res.send(newActivity) 
+  
+  }catch (error) {
+     console.log ('No anda createActivity')
+ }
 }
 
 module.exports = {
--- api/src/controllers/CountriesControl.js
+++ api/src/controllers/CountriesControl.js
@@ -1,17 +1,28 @@
 const axios = require ('axios');
 const {allInfoNecCountries} = require ('./DataCountries')
-const {Country} = require ('../models/Country')
+const {Country, Activity} = require ('../db')
 
-const getAllAndByName = async (req, res, next) => {
+const getAllAndByName = async (req, res, next) => {     //en esta... tengo que incluir actividades y población para los filtros aunque no se muetren en las cards?
     try {
-       const culo = await allInfoNecCountries (); 
+       const {name} = req.query 
+       const allInfo = await allInfoNecCountries (); 
+       const infoRp = await allInfo.map (e => {
+           return {
+            id: e.id,
+            name: e.name,
+            flags: e.flags,
+            region: e.region
+           }
+       })
 
-        res.send (culo);    
-        // const db = await allInfoNecCountries()  
-    
-    console.log(culo)
-
-    // res.send (db)
+       if (name){
+           let filterName = infoRp.filter (n => n.name.toLowerCase().includes(name.toString().toLowerCase())) // esto es fijate si el nombre que tenemos coincide en algun punto con el que recibo por query
+           filterName.length > 0?
+           res.send (filterName):
+           res.send ('No se ha encontrado el país, intentelo nuevamente')
+       } else {
+           res.send(infoRp)
+       } 
 
     }catch (error) {
         next (error)
@@ -19,11 +30,70 @@ const getAllAndByName = async (req, res, next) => {
 
  
 }
+ 
+//ANDA TODO - VER MÁS TEORÍA A MEDIDA QUE PASAN LOS DÍAS.
+const getOneById = async (req, res, next) => {
+    const {id} = req.params
+    try{
+        if (id){
+           let busqueda = await Country.findAll({
+                where: {id: id},
+                include: {
+                    model: Activity,
+                    attributes: ["name", "id", "difficulty", "duration", "season"],
+                    through: {
+                        attributes: []
+                    }
+                }
+            })
+            console.log(busqueda)
+            res.send(busqueda);
 
-const getOneById = () => {
+        }
+
+        
+    }catch (error) {
+        next(error)
+    }
 
 }
 
+// const getById = async (req, res, next) => {
+//     const {id} = req.params  
+//     try{
+//     const allInfoById = await getoneByIdApi(id);
+//     if (id.length < 4) {
+//             let infoNecId = allInfoById?.map (el => {
+//                 let weigth1 = '';
+//                 if (el.weight.metric === "NaN") {
+//                    weigth1 = "27 - 34"
+//                 } else if (el.weight.metric.split(" - ")[0] === "NaN") {
+//                     weigth1 = "6 - 8"
+//                 } else {
+//                     weigth1 = el.weight.metric
+//                 }
+//                 return {
+//                     image: el.image.url,
+//                     name: el.name,
+//                     temperament: el.temperament? el.temperament : 'Perrito sin temperamento',
+//                     weight: weigth1,
+//                     height: el.height.metric,
+//                     life_span: el.life_span
+//                 }
+//             })
+//             infoNecId.length === 0?
+//             res.status(404).send ('No se encontró el perrito requerido, intentelo de nuevo'):
+//             res.send (infoNecId)
+//         } else {
+//             let infoDbById = await getDogsForIdDb (id);
+//             console.log (infoDbById)            
+//             return res.json(infoDbById)
+//         }
+//     } catch (error) {
+//         next (error)
+//     }
+
+// };
 
 module.exports = {
     getAllAndByName, 
