d6608ad9f5cc53c7ba363a9c420b68052d51b694
Your Name <you@example.com>
2022-04-14 19:21:18 -0300

    redux in process

---
+++ client/src/Redux/actions.js
@@ -0,0 +1,22 @@
+const GET_ALL_COUNTRIES = 'GET_ALL_COUNTRIES';
+const SEARCH_BY_NAME = 'SEARCH_BY_NAME';
+const FILTER_BY_ACTIVITY = 'FILTER_BY_ACTIVITY';
+const FILTER_BY_REGION = 'FILTER_BY_REGION ';
+const ORDER_BY_NAME_AZ = 'ORDER_BY_NAME_AZ';
+const ORDER_BY_NAME_ZA = 'ORDER_BY_NAME_ZA ';
+const ORDER_BY_POPULATION_ASC = 'ORDER_BY_POPULATION_ASC ';
+const ORDER_BY_POPULATION_DES = 'ORDER_BY_POPULATION_DES';
+const GET_ALL_DETAIL = 'GET_ALL_DETAIL';
+const GET_ACTITIVIIES = 'GET_ACTITIVIIES'; //ACÁ HAY UN TEMA, NO TENGO NINGUNA RUTA, SOLO TENGO EL POST. LAS DEBERÍA TRAER DEL POST PARA EL ORDENAMIENTO...VER ESO
+const POST_ACTIVITY = 'POST_ACTIVITY'
+
+const LOCAL_HOST = 'http://localhost:3001/'
+const RUTA_GET_COUNTRY = 'http://localhost:3001/countries/get'
+const RUTA_POST_ACTIVITY = 'http://localhost:3001/activities/create'
+
+// Actions Home : allInfoCountry, searchByName, filterByActivity, 
+//filterByRegion, orderByNameAZ, orderByNameZA, orderByPopulationAsc, orderByPopulationDes.
+
+// Actions ruta Detail: allInfoDetail, getActivities
+
+// Actions ruta Create : postActivity
---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,14 @@
+import { GET_ALL_COUNTRIES, SEARCH_BY_NAME, FILTER_BY_ACTIVITY, FILTER_BY_REGION, ORDER_BY_NAME_AZ, ORDER_BY_NAME_ZA, ORDER_BY_POPULATION_ASC,
+ORDER_BY_POPULATION_DES, GET_ALL_DETAIL, GET_ACTITIVIIES, POST_ACTIVITY} from './actions' 
+
+const initialState = {
+
+}
+
+const rootReducer = (state=initialState, action) => {
+    switch (action.type) {
+
+    }
+}
+
+export default rootReducer;
