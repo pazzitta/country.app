09ab14086de4a23ec3d6cb49531721f12a3de6cb
Your Name <you@example.com>
2022-04-20 11:16:34 -0300

    structure detail in process 2

--- client/src/Components/CountryAndActivity/countryAndActivity.jsx
+++ client/src/Components/CountryAndActivity/countryAndActivity.jsx
@@ -10,12 +10,52 @@ export default function BodyDetail () {
     return (
         <div>
             <div className="cajaCardsActDet">
-               <div className="nameActiDet">ACTIVIDADES</div>
+               <div className="nameActiDet">ACTIVIDADES TURÍSTICAS</div>
                <CardsActivity/>
             </div>
 
             <div className="cajaCountriesDet">
-               <div>solo el detalle del país</div> 
+               
+               <div className="boxPasaporteDetail">
+
+                  <div>Nombre country</div>
+                  
+                  <div className="boxImagenDet">
+                     <div>Imagen(flasg)</div>
+                     <div>ID3</div>
+                  </div>
+               
+                  <div className="boxInfoCountDet">
+
+                    <div>
+                        <div>Continenete:</div>
+                        <div>Nombre contienente</div>
+                     </div>
+
+                     <div>
+                        <div>Subregión:</div>
+                        <div>Nombre subregión</div>
+                     </div>
+
+                     <div>
+                        <div>Capital:</div>
+                        <div>Nombre capital</div>
+                     </div>
+
+                     <div>
+                        <div>Población:</div>
+                        <div>Cant de población</div>
+                     </div>
+
+                     <div>
+                        <div>Área:</div>
+                        <div>área en m2</div>
+                     </div>
+               
+                  </div>
+               
+               </div>
+            
             </div>
             
             
