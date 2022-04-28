64038bcece615315c65f822727ac3cb3205b16fb
Your Name <you@example.com>
2022-04-28 19:43:35 -0300

    revisión landing ok

--- client/src/Components/LandingPage/landing.jsx
+++ client/src/Components/LandingPage/landing.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import './landing.css';
 import Videofondo from '../../video/prueba3.mp4'
 
-//Tengo un tema en el frame final de vide, cuando hace el loop queda feito, después hay algo en el localizador de australia y quizá los aviones tenegan que ser mas grandes.
-//SI CUANDO TERMINE TODA LA FUNCIONALIDAD HACER ESTE VIDEO, POR EL MOMENTO QUEDA ASÍ!
 
 export default function LandingPage () {
   return ( 
-    <div >
+    <div className="cajadivLP">
       <video className="videoLanding"  autoPlay ="autoplay" loop muted>
-         <source src={Videofondo} type= "video/mp4" />
+         <source  src={Videofondo} type= "video/mp4" />
       </video>
-      {/* HAY UN LINK EN EL FONDO DE LA PÁG PROBABLEMENTE SEA ESO LO QUE JODE */}
           <NavLink className="landingCoun"  to= '/home' id='click'> 
              <button className="botonHomeLP"></button>
           </NavLink>
